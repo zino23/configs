fbd52cad4e4784656491228f462f644cd731b21c
zino <theforgotten23623@gmail.com>
2023-07-02 13:26:51 +0800

    Add a new shell script to download some softwares

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## PlantUML
+echo "Installing PlantUML..."
+
+# Install dependency java 
+if ! which java &> /dev/null; then
+    echo "Installing java with Homebrew..."
+    brew install java
+fi
+
+if ! java -version &> /dev/null; then
+    # Depend on standard Homebrew path
+    sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+fi
+
+# Install dependency graphviz
+if ! which dot &> /dev/null; then
+    echo "Installling graphviz with Homebrew..."
+    brew install graphviz
+fi
+
+# Finally, install PlantUML
+if ! which plantuml &> /dev/null; then
+    echo "Installling PlantUML with Homebrew..."
+    brew install plantuml
+fi
+
+echo "PlantUML successfully installed!"
+
+## Rime Configuration Manager
+# echo "Installing Rime Configuration Manager plum..."
+# curl -fsSL https://raw.githubusercontent.com/rime/plum/master/rime-install | bash
+
+
