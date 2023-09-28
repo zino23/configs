a4d2a36366401f6f16b7088f984a180283f4f946
zino <theforgotten23623@gmail.com>
2023-09-28 17:08:37 +0800

    [fish] Add support for vterm

--- fish/config.fish
+++ fish/config.fish
@@ -139,3 +139,18 @@ if test ! -d "$HOME/.tmux/plugins/tpm"
     echo 'Start to installing Tmux Plugin Manager tpm ...'
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 end
+
+## vterm
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+set fish_greeting
