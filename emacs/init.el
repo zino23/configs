c86d2a44f96c990dccdcc5a94049bf9756ab585e
zino <theforgotten23623@gmail.com>
2023-12-18 15:00:11 +0800

    emacs,orgmode: a dirty way to support backtick as inline code delimiters

--- emacs/init.el
+++ emacs/init.el
@@ -1415,7 +1415,84 @@ respectively."
   ;; NOTE: Use absolute links to make `org-babel-detangle' work when the filename of
   ;; the tangled file is transformed in `org-babel-post-tangle-hook', namely
   ;; `zino/org-babel-tangle-dir'.
-  (setq org-babel-tangle-use-relative-file-links nil))
+  (setq org-babel-tangle-use-relative-file-links nil)
+
+  :config
+  ;;  Make backtick work as inline `org-vertatim' delimiter.
+  (add-to-list 'org-emphasis-alist '("`" org-verbatim verbatim))
+  (defun org-set-emph-re (var val)
+    "Set variable and compute the emphasis regular expression."
+    (set-default-toplevel-value var val)
+    (when (and (boundp 'org-emphasis-alist)
+	             (boundp 'org-emphasis-regexp-components)
+	             org-emphasis-alist org-emphasis-regexp-components)
+      (pcase-let*
+	        ((`(,pre ,post ,border ,body ,nl) org-emphasis-regexp-components)
+	         (body (if (<= nl 0) body
+		               (format "%s*?\\(?:\n%s*?\\)\\{0,%d\\}" body body nl)))
+	         (template
+	          (format (concat "\\([%s]\\|^\\)" ;before markers
+			                      "\\(\\([%%s]\\)\\([^%s]\\|[^%s]%s[^%s]\\)\\3\\)"
+			                      "\\([%s]\\|$\\)") ;after markers
+		                pre border border body border post)))
+        (setq org-emph-re (format template "*/_+"))
+        ;; Add backtick.
+        (setq org-verbatim-re (format template "=~`")))))
+
+  (defun org-do-emphasis-faces (limit)
+    "Run through the buffer and emphasize strings."
+    (let ((quick-re (format "\\([%s]\\|^\\)\\([~=`*/_+]\\)"
+			                      (car org-emphasis-regexp-components))))
+      (catch :exit
+        (while (re-search-forward quick-re limit t)
+	        (let* ((marker (match-string 2))
+                 ;; Add backtick.
+	               (verbatim? (member marker '("~" "=" "`"))))
+	          (when (save-excursion
+		                (goto-char (match-beginning 0))
+		                (and
+		                 ;; Do not match table hlines.
+		                 (not (and (equal marker "+")
+			                         (org-match-line
+			                          "[ \t]*\\(|[-+]+|?\\|\\+[-+]+\\+\\)[ \t]*$")))
+		                 ;; Do not match headline stars.  Do not consider
+		                 ;; stars of a headline as closing marker for bold
+		                 ;; markup either.
+		                 (not (and (equal marker "*")
+			                         (save-excursion
+			                           (forward-char)
+			                           (skip-chars-backward "*")
+			                           (looking-at-p org-outline-regexp-bol))))
+		                 ;; Match full emphasis markup regexp.
+		                 (looking-at (if verbatim? org-verbatim-re org-emph-re))
+		                 ;; Do not span over paragraph boundaries.
+		                 (not (string-match-p org-element-paragraph-separate
+					                                (match-string 2)))
+		                 ;; Do not span over cells in table rows.
+		                 (not (and (save-match-data (org-match-line "[ \t]*|"))
+			                         (string-match-p "|" (match-string 4))))))
+	            (pcase-let ((`(,_ ,face ,_) (assoc marker org-emphasis-alist))
+			                    (m (if org-hide-emphasis-markers 4 2)))
+	              (font-lock-prepend-text-property
+	               (match-beginning m) (match-end m) 'face face)
+	              (when verbatim?
+		              (org-remove-flyspell-overlays-in
+		               (match-beginning 0) (match-end 0))
+                  (when (and (org-fold-core-folding-spec-p 'org-link)
+                             (org-fold-core-folding-spec-p 'org-link-description))
+                    (org-fold-region (match-beginning 0) (match-end 0) nil 'org-link)
+                    (org-fold-region (match-beginning 0) (match-end 0) nil 'org-link-description))
+		              (remove-text-properties (match-beginning 2) (match-end 2)
+					                                '(display t invisible t intangible t)))
+	              (add-text-properties (match-beginning 2) (match-end 2)
+				                             '(font-lock-multiline t org-emphasis t))
+	              (when (and org-hide-emphasis-markers
+			                     (not (org-at-comment-p)))
+		              (add-text-properties (match-end 4) (match-beginning 5)
+				                               '(invisible t))
+		              (add-text-properties (match-beginning 3) (match-end 3)
+				                               '(invisible t)))
+	              (throw :exit t)))))))))
 
 (use-package org
   :hook
@@ -1426,7 +1503,7 @@ respectively."
 (use-package org
   :custom
   (org-ellipsis " ▾")
-  (org-hide-emphasis-markers nil)
+  (org-hide-emphasis-markers t "with `org-appear' it is usable. but need to figure out how to integrate `")
   (org-M-RET-may-split-line nil)
   (org-list-allow-alphabetical t)
   (org-return-follows-link t)
